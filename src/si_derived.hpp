899294510c3251b76a50d4fc5f44866ccd0f17e2
Clang Robot <robot@example.com>
2020-11-15 16:27:44 +0000

    Committing clang-format changes

--- src/si_derived.hpp
+++ src/si_derived.hpp
@@ -11,61 +11,95 @@ namespace Si {
 namespace Derived {
 /// @tparam T Any number type such as float, double or long double used as a
 /// container
-template <typename T> using Hertz = decltype(std::declval<Internal::Dimensionless<T>>()/std::declval<Base::Second<T>>());
+template <typename T>
+using Hertz = decltype(std::declval<Internal::Dimensionless<T>>() /
+                       std::declval<Base::Second<T>>());
 /// @tparam T Any number type such as float, double or long double used as a
 /// container
-template <typename T> using Newton = decltype(std::declval<Base::Kilogram<T>>()*std::declval<Base::Meter<T>>()/(std::declval<Base::Second<T>>()*std::declval<Base::Second<T>>()));
+template <typename T>
+using Newton = decltype(
+    std::declval<Base::Kilogram<T>>() * std::declval<Base::Meter<T>>() /
+    (std::declval<Base::Second<T>>() * std::declval<Base::Second<T>>()));
 /// @tparam T Any number type such as float, double or long double used as a
 /// container
-template <typename T> using Pascal = decltype(std::declval<Newton<T>>()/(std::declval<Base::Meter<T>>()* std::declval<Base::Meter<T>>()));
+template <typename T>
+using Pascal =
+    decltype(std::declval<Newton<T>>() /
+             (std::declval<Base::Meter<T>>() * std::declval<Base::Meter<T>>()));
 /// @tparam T Any number type such as float, double or long double used as a
 /// container
-template <typename T> using Joule = decltype(std::declval<Base::Meter<T>>()*std::declval<Newton<T>>());
+template <typename T>
+using Joule =
+    decltype(std::declval<Base::Meter<T>>() * std::declval<Newton<T>>());
 /// @tparam T Any number type such as float, double or long double used as a
 /// container
-template <typename T> using Watt = decltype(std::declval<Joule<T>>()/std::declval<Base::Second<T>>());
+template <typename T>
+using Watt =
+    decltype(std::declval<Joule<T>>() / std::declval<Base::Second<T>>());
 /// @tparam T Any number type such as float, double or long double used as a
 /// container
-template <typename T> using Coulomb = decltype(std::declval<Base::Second<T>>()*std::declval<Base::Ampere<T>>());
+template <typename T>
+using Coulomb =
+    decltype(std::declval<Base::Second<T>>() * std::declval<Base::Ampere<T>>());
 /// @tparam T Any number type such as float, double or long double used as a
 /// container
-template <typename T> using Volt = decltype(std::declval<Watt<T>>()/std::declval<Base::Ampere<T>>());
+template <typename T>
+using Volt =
+    decltype(std::declval<Watt<T>>() / std::declval<Base::Ampere<T>>());
 /// @tparam T Any number type such as float, double or long double used as a
 /// container
-template <typename T> using Farad = decltype(std::declval<Coulomb<T>>()/std::declval<Volt<T>>());
+template <typename T>
+using Farad = decltype(std::declval<Coulomb<T>>() / std::declval<Volt<T>>());
 /// @tparam T Any number type such as float, double or long double used as a
 /// container
-template <typename T> using Ohm = decltype(std::declval<Volt<T>>()/std::declval<Base::Ampere<T>>());
+template <typename T>
+using Ohm = decltype(std::declval<Volt<T>>() / std::declval<Base::Ampere<T>>());
 /// @tparam T Any number type such as float, double or long double used as a
 /// container
-template <typename T> using Siemens = decltype(std::declval<Base::Ampere<T>>()/std::declval<Volt<T>>());
+template <typename T>
+using Siemens =
+    decltype(std::declval<Base::Ampere<T>>() / std::declval<Volt<T>>());
 /// @tparam T Any number type such as float, double or long double used as a
 /// container
-template <typename T> using Weber = decltype(std::declval<Joule<T>>()/std::declval<Base::Ampere<T>>());
+template <typename T>
+using Weber =
+    decltype(std::declval<Joule<T>>() / std::declval<Base::Ampere<T>>());
 /// @tparam T Any number type such as float, double or long double used as a
 /// container
-template <typename T> using Tesla = decltype(std::declval<Newton<T>>()/(std::declval<Base::Ampere<T>>()*std::declval<Base::Meter<T>>()));
+template <typename T>
+using Tesla =
+    decltype(std::declval<Newton<T>>() / (std::declval<Base::Ampere<T>>() *
+                                          std::declval<Base::Meter<T>>()));
 /// @tparam T Any number type such as float, double or long double used as a
 /// container
-template <typename T> using Henry = decltype(std::declval<Weber<T>>()/std::declval<Base::Ampere<T>>());
+template <typename T>
+using Henry =
+    decltype(std::declval<Weber<T>>() / std::declval<Base::Ampere<T>>());
 /// @tparam T Any number type such as float, double or long double used as a
 /// container
 template <typename T> using Lumen = Base::Candela<T>;
 /// @tparam T Any number type such as float, double or long double used as a
 /// container
-template <typename T> using Lux = decltype(std::declval<Lumen<T>>()/(std::declval<Base::Meter<T>>()*std::declval<Base::Meter<T>>()));
+template <typename T>
+using Lux =
+    decltype(std::declval<Lumen<T>>() /
+             (std::declval<Base::Meter<T>>() * std::declval<Base::Meter<T>>()));
 /// @tparam T Any number type such as float, double or long double used as a
 /// container
 template <typename T> using Becquerel = Hertz<T>;
 /// @tparam T Any number type such as float, double or long double used as a
 /// container
-template <typename T> using Gray = decltype(std::declval<Joule<T>>()/std::declval<Base::Kilogram<T>>());
+template <typename T>
+using Gray =
+    decltype(std::declval<Joule<T>>() / std::declval<Base::Kilogram<T>>());
 /// @tparam T Any number type such as float, double or long double used as a
 /// container
 template <typename T> using Sievert = Gray<T>;
 /// @tparam T Any number type such as float, double or long double used as a
 /// container
-template <typename T> using Katal = decltype(std::declval<Base::Mole<T>>()/std::declval<Base::Second<T>>());
+template <typename T>
+using Katal =
+    decltype(std::declval<Base::Mole<T>>() / std::declval<Base::Second<T>>());
 /// @brief Use this namespace to enable literals for base units
 namespace Literals {
 /// @brief _m long double literal for hertz
@@ -100,7 +134,8 @@ Volt<long double> operator"" _V(long double value) {
 Farad<long double> operator"" _F(long double value) {
   return Farad<long double>(value);
 }
-/// @brief _cd long double literal for ohm(not as defined by standard because of non-ASCII symbol)
+/// @brief _cd long double literal for ohm(not as defined by standard because of
+/// non-ASCII symbol)
 Ohm<long double> operator"" _Ohm(long double value) {
   return Ohm<long double>(value);
 }
