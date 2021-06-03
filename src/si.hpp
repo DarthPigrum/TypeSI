0b8cc936cf97cda4f517e280ba7f7e6103ef5b19
DarthPigrum <darthpigrum@gmail.com>
2021-06-04 00:46:24 +0300

    removing files for review

--- src/si.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-/// @file   si.hpp
-/// @brief  Main header
-#include "si_base.hpp"
-#include "si_derived.hpp"
-#include "si_math.hpp"
-#include "si_prefix.hpp"
-#include "si_special.hpp"
--- src/si_utilities.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-/// @file   si_utilities.hpp
-/// @brief  Internal instruments of the library, that does not involve core
-/// logic
-namespace Si {
-namespace Internal {
-template <typename T> const T &implicit_cast(const T &t) { return t; }
-} // namespace Internal
-} // namespace Si
